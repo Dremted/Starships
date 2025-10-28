77f79fe2c8b643b29bc7cca58dcc2abf73dd1991
Dremted <Dremted@gmail.com>
2025-10-28 20:32:01 +0500

    engry

--- Assets/SpawnEnemy.cs
+++ Assets/SpawnEnemy.cs
@@ -5,4 +5,26 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
     public Transform[] pointsSpawn;
+    public GameObject prefabEnemy;
+
+    public float timeToSpawn = 3f;
+    private float timerSpawn = 3f;
+
+
+    private void Update()
+    {
+        timerSpawn -= Time.deltaTime;
+
+        if (timerSpawn <= 0)
+            Spawn();
+    }
+
+    public void Spawn()
+    {
+        foreach (var point in pointsSpawn) 
+        {
+            Instantiate(prefabEnemy, point.position, Quaternion.identity);
+            timerSpawn = timeToSpawn;
+        }
+    }
 }
